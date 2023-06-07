f9f646b2d8a57354c2d4f16f7d9e8c707a5cccb8
DanielProkopDev <daniel.prokop.dev@gmail.com>
2023-06-07 19:40:23 +0100

    Your commit message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ import GUI.MainFrame;
 
 import javax.swing.*;
 
-public class Main {
-
-
+public class Main implements Runnable {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    @Override
+    public void run() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 MainFrame mainFrame = new MainFrame();
